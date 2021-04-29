65aabe45620d36d90b53f7e24e44d0fa65ca42e9
octahate <geli961212@hotmail.com>
2021-04-29 10:26:01 +0200

    first task done

---
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/addigits/AddDigits.java
@@ -0,0 +1,20 @@
+package hu.nive.ujratervezes.kepesitovizsga.addigits;
+
+public class AddDigits {
+
+    public static String NUMBERS= "0123456789";
+
+    public int addDigits(String input){
+        if (input == null || input.isBlank()){
+            return -1;
+        }
+        int counter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            String currentNumberAsString = input.substring(i,i+1);
+            if (NUMBERS.contains(currentNumberAsString)) {
+                counter+=Integer.parseInt(currentNumberAsString);
+            }
+        }
+        return counter;
+    }
+}
